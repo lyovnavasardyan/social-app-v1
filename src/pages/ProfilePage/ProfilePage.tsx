c8d50feeebc2f968867aba1b49597e0b4c6cdbf5
Levon Navasardyan <levonnavasardyan7@gmail.com>
2024-06-25 05:44:19 -0500

    Updated details button in profile page

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -5,10 +5,8 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { Rootstate } from '../../store/store';
 import { ToastContainer } from 'react-toastify';
 import PhotosPage from '../PhotosPage/PhotosPage';
-import ProfilePhoto from '../ProfilePhoto/profilePhoto';
 import CoverPhoto from '../CoverPhoto/coverPhoto';
 import { updateToast } from './toastupdate';
-import { useNavigate } from 'react-router-dom';
 import Details from './Details/Details';
 
 import './style.css';
@@ -26,7 +24,6 @@ interface FormValues {
 const ProfilePage: React.FC = () => {
     const { error } = useSelector((state: Rootstate) => state.updatedData);
     const dispatch = useDispatch();
-    const navigate = useNavigate();
     const [detailsButton, setDetailsButton] = useState(false);
 
 
@@ -45,23 +42,27 @@ const ProfilePage: React.FC = () => {
 
     return (
         <div className="profile_page_div">
-            {/* <h2>Profile</h2> */}
             <CoverPhoto />
+         
             {detailsButton ? (
-                <Details
-                 handleSubmit={handleSubmit}
-                 onSubmit={onSubmit}
-                 error={error}
-                 register={register}
-                 errors={errors}
-                  />
-            ) : (
+                <div className="details-container">
+                    <Details
+                        handleSubmit={handleSubmit}
+                        onSubmit={onSubmit}
+                        error={error}
+                        register={register}
+                        errors={errors}
+                    />
+                    <button className="close-button" onClick={() => setDetailsButton(false)}>Close</button>
+                </div>
+            ):(
                 <button className="details-button" onClick={detailsButtonHandler}>{detailsButton ? "Save" : "Details"}</button>
             )}
             <PhotosPage />
             <ToastContainer />
         </div>
     );
+    
 }
 
 export default ProfilePage;
